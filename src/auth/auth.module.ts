cdc835ae2e945d0975dd1ec5d09e5593dded1500
jl-gosewisch <lennion13@gmail.com>
2022-08-12 00:37:33 +0200

    Added AuthModule and main.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./strategies/local.strategy";
 import { AuthController } from './auth.controller';
+import { HashService } from "./hashing/hashing.service";
+import { UserService } from "./user/user.service";
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+    imports: [PassportModule],
+    providers: [AuthService, LocalStrategy, HashService, UserService],
+    controllers: [AuthController]
 })
 export class AuthModule {}
