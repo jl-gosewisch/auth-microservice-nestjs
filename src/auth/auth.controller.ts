2b9e5988ee988d9b1b285767ce78c1ac8943ef6d
jl-gosewisch <lennion13@gmail.com>
2022-08-12 01:35:46 +0200

    Added login and signup routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user/user.service';
+import { User, User as UserModel } from '@prisma/client';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller()
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
-  }
+    constructor( private userService: UserService) {}
+
+    @UseGuards(LocalAuthGuard)
+    @Post('auth/login')
+    async login(@Request() req) {
+        return req.user;
+    };
+
+    @Post('signup')
+    async signupUser(
+        @Body() userData: { email: string; passwort: string },
+    ): Promise<UserModel> {
+        return this.userService.createUser(userData);
+    }
 }
 
